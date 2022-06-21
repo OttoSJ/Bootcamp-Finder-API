e85f0488abbb2bf8111c2128efa69c0172363a95
Otto Jones <ottosjonesjr@gmail.com>
2022-06-21 19:32:40 +0300

    databse setup

--- server.js
+++ server.js
@@ -2,12 +2,20 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-// Route files
-const bootcamp = require('./routes/bootcamps')
-const logger = require('./middleware/logger')
+const connectDB = require('./config/db')
+const colors = require('colors')
+
 // Load env vars
 dotenv.config({ path: './config/config.env' })
 const PORT = process.env.PORT || 5000
+
+// Connect to database
+connectDB()
+
+// Route files
+const bootcamp = require('./routes/bootcamps')
+
+const logger = require('./middleware/logger') //example of custom middleware (not in use)
 
 // Configure endpoint prefix's
 const app = express()
@@ -20,5 +28,8 @@ app.use('/api/v1/bootcamps', bootcamp)
 // Run server
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.gray
+      .underline
+  )
 )
