f3ec45085e9b410fc6343ca655616e2f0fae671b
Otto Jones <ottosjonesjr@gmail.com>
2022-06-23 17:52:07 +0300

    added basic crud routes and tested with database. added middleware geolocation and error handling

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const connectDB = require('./config/db')
 const colors = require('colors')
+const errorHandler = require('./middleware/error')
+// TO RUN SEVER = NPM RUN DEV
 
 // Load env vars
 dotenv.config({ path: './config/config.env' })
@@ -15,15 +17,21 @@ connectDB()
 // Route files
 const bootcamp = require('./routes/bootcamps')
 
-const logger = require('./middleware/logger') //example of custom middleware (not in use)
+// const logger = require('./middleware/logger') //example of custom middleware (not in use)
 
 // Configure endpoint prefix's
 const app = express()
 
+// Body parser
+app.use(express.json())
+
+// Dev Middleware
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
 
 // Mount Routers
 app.use('/api/v1/bootcamps', bootcamp)
+
+app.use(errorHandler) // ** Must be under the routes **
 
 // Run server
 app.listen(
